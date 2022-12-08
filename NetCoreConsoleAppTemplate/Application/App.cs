7267e2ff2aecf7d18ff8bb320f79e3d359cc8782
glitchkn1ght <54705224+glitchkn1ght@users.noreply.github.com>
2022-12-08 14:05:15 +0000

    Add project files.

---
+++ NetCoreConsoleAppTemplate/Application/App.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace NetCoreConsoleAppTemplate.Application
+{
+    public class App
+    {
+        private readonly ILogger<App> Logger;
+
+        public App(ILogger<App> logger)
+        {
+            this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Run(string[] args)
+        {
+            try
+            {
+                this.Logger.LogInformation($"Operation=Run(App), Status=Success, Message=Application  has started successfully.");
+            }
+
+            catch (Exception ex)
+            {
+                this.Logger.LogError($"Operation=RunGame(App), Status=Failure, Message={ex.Message}");
+            }
+        }
+    }
+}
---
+++ NetCoreConsoleAppTemplate/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreConsoleAppTemplate.Application;
+using Serilog;
+using System;
+
+namespace NetCoreConsoleAppTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // entry point to run app
+            serviceProvider.GetService<App>().Run(args);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // build config
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables()
+            .Build();
+
+            // configure logging
+            services.AddLogging(builder => builder.AddSerilog(
+                new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger()))
+                .BuildServiceProvider();
+
+            //Configure application settings/DI
+
+            // add app
+            services.AddTransient<App>();
+        }
+    }
+}
